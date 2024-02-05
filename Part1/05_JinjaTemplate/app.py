ba2318be4295effe5093e31f86fecb10e07622f4
lss018000 <lss018000@gmail.com>
2024-02-05 13:19:59 +0900

    20240205_1

--- Part1/05_JinjaTemplate/app.py
+++ Part1/05_JinjaTemplate/app.py
@@ -11,9 +11,15 @@ def index():
         'item_list':["Item1", "Item2", "Item3"]
     }
     
+    users = [
+        {"username": "traveler", "name": "Alex"},
+        {"username": "photographer", "name": "Sam"},
+        {"username": "gourmet", "name": "Chris"}
+    ]
+    
     #(1)rendering할 html 파일명 입력
     #(2)html로 넘겨줄 데이터 입력
-    return render_template('index.html', data=data)
+    return render_template('index.html', data=data, users = users)
 
 if __name__ == "__main__":
     app.run(debug=True)
